aabb65125f4ce6568a354aee74b27d2d2374f25d
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-09-20 11:05:36 +0200

    Adds fake data

--- src/fort-studio/Experiment.cpp
+++ src/fort-studio/Experiment.cpp
@@ -5,20 +5,57 @@
 
 Experiment::Experiment( QObject * parent)
 	: QObject(parent)
-	, d_experiment(new fort::myrmidion::priv::Experiment())
 	, d_modified(false) {
-	if (parent == NULL ) {
-
-	}
+	reset();
 }
 
 Experiment::~Experiment() {
 }
 
 
-
 void Experiment::reset() {
 	d_experiment = fort::myrmidion::priv::Experiment::Ptr(new fort::myrmidion::priv::Experiment());
+	fort::myrmidion::pb::AntMetadata a;
+
+	a.set_id(1);
+	auto idtf = a.add_marker();
+	idtf->set_startvalidframe(0);
+	auto m = idtf->mutable_marker();
+	m->set_id(0);
+	m->set_x(1.5);
+	m->set_y(-2.0);
+	m->set_theta(42.3);
+
+	d_experiment->d_ants.push_back(a);
+	a.Clear();
+	a.set_id(4236);
+	d_experiment->d_ants.push_back(a);
+	a.Clear();
+
+	a.set_id(562021);
+	idtf = a.add_marker();
+	idtf->set_startvalidframe(0);
+	m = idtf->mutable_marker();
+	m->set_id(234);
+	m->set_x(-1.5);
+	m->set_y(2.0);
+	m->set_theta(-42.3);
+	idtf = a.add_marker();
+	idtf->set_startvalidframe(19674);
+	m = idtf->mutable_marker();
+	m->set_id(234);
+	m->set_x(2);
+	m->set_y(-12.0);
+	m->set_theta(89.3);
+	idtf = a.add_marker();
+	idtf->set_startvalidframe(35894);
+	m = idtf->mutable_marker();
+	m->set_id(589);
+	m->set_x(-5.5);
+	m->set_y(-12.0);
+	m->set_theta(0.3);
+
+
 	markModified(false);
 }
 
