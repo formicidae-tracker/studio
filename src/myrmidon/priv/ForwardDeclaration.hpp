70088290afda7a1135a5f4a944976c9e1943fff3
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-10-30 16:00:12 +0100

    Documents forwards and typedef declarations

--- src/myrmidon/priv/ForwardDeclaration.hpp
+++ src/myrmidon/priv/ForwardDeclaration.hpp
@@ -19,13 +19,24 @@ class Identification;
 class Identifier;
 class FramePointer;
 
+// Forward decalation for an <Experiment::Ptr>
 typedef std::unique_ptr<Experiment>     ExperimentPtr;
+// Forward decalation for an <Ant::Ptr>
 typedef std::shared_ptr<Ant>            AntPtr;
+// Forward decalation for an <Identification::Ptr>
 typedef std::shared_ptr<Identification> IdentificationPtr;
-typedef std::vector<IdentificationPtr>  IdentificationList;
+// Forward decalation for an <Identifier::Ptr>
 typedef std::shared_ptr<Identifier>     IdentifierPtr;
+// Forward decalation for an <FramePointer::Ptr>
 typedef std::shared_ptr<const FramePointer>   FramePointerPtr;
+
+
+// A Map of <Ant> identified by their <Ant::ID>
 typedef std::unordered_map<fort::myrmidon::Ant::ID,AntPtr> AntByID;
+
+// A List of <Identification>
+typedef std::vector<IdentificationPtr>  IdentificationList;
+
 
 } // namespace priv
 
