8035043f82cf2755818f6d1c19d04b215ef88510
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-10-30 17:58:52 +0100

    Adds a lot of individual documentation

--- src/myrmidon/priv/Ant.hpp
+++ src/myrmidon/priv/Ant.hpp
@@ -14,26 +14,72 @@ namespace myrmidon {
 
 namespace priv {
 
+// The Object of Interest of any Experiment
+//
+// <priv::Ant> are the unique object of interest of an <Experiment> they are
+// uniquely identified by their <myrmidon::Ant::ID>.
+//
+// <priv::Identification> relates an <priv::Ant> to a
+// <myrmidon::TagID>. <Identifier::Identify> can be used to retrieve
+// the <priv::Identification> associated to a <TagID> if it exist in any
+// point in time of an <Experiment> defined by a <FramePointer>.
+//
+// An <priv::Ant> could have multiple <priv::Identification>
+// associated with it, but they should not overlap in
+// time. <Ant::SortAndCheckIdentifications> could be used to ensure
+// this.
 class Ant {
 public:
+	// A pointer to an Ant
 	typedef std::shared_ptr<Ant> Ptr;
+
+	// The Constructor for an Ant
 	Ant(uint32_t ID);
+
+	// Its destructor
 	~Ant();
 
+	// The Identification::List associated with this priv::Ant.
+	//
+	// A sorted <Identification::List> associated with this
+	// <priv::Ant>. Adding and removing of <Identification> have to be
+	// respectively performed using <Identifier::AddIdentification>
+	// and <Identifier::RemoveIdentification>.
 	const Identification::List & Identifications() const;
+
+	// Sorts and checks the Identification associated with this Ant.
+	//
+	// Sorts and checks the <Identification> associated with this
+	// <priv::Ant> and throws <OverlappingIdentification> if any
+	// overlaps in time.
 	void SortAndCheckIdentifications();
 
-
+	// Gets the Unique ID of the Ant
+	//
+	// <Ant> managed by the same <Identifier> are uniquely identified
+	// by an Unique ID.
+	//
+	// @return the unique ID of the Ant
 	fort::myrmidon::Ant::ID ID() const {
 		return d_ID;
 	}
 
+	// Get a formatted ID.
+	//
+	// @return a string in the format "0xabcd"
 	const std::string & FormattedID() const {
 		return d_IDStr;
 	}
 
+	// Formats an Ant::ID to "0xabcd"
+	//
+	// @return the <myrmidon::Ant::ID> formatted in hexadecimal "0xabcd"
 	static std::string FormatID(fort::myrmidon::Ant::ID ID);
 
+	// C++ shenanigans
+	//
+	// C++ shenanigan class to give restricted unlimited access to the
+	// <Identification::List> to the <Identifier> class.
 	class Accessor {
 	private:
 		static Identification::List & Identifications(Ant & a);
--- src/myrmidon/priv/Experiment.hpp
+++ src/myrmidon/priv/Experiment.hpp
@@ -8,6 +8,8 @@
 #include "TrackingDataDirectory.hpp"
 #include "Identifier.hpp"
 
+#include "ForwardDeclaration.hpp"
+
 namespace fort {
 namespace myrmidon {
 
@@ -18,39 +20,116 @@ class TrackingDataDirectory;
 
 namespace priv {
 
-class Ant;
-typedef std::shared_ptr<Ant> AntPtr;
-typedef std::unordered_map<fort::myrmidon::Ant::ID,AntPtr> AntByID;
 
 using namespace fort::myrmidon;
 
-
-
+// Entry point of the myrmidon private interface
+//
+// An <priv::Experiment> is responsible to hold the links to the
+// <TrackingDataDirectory> and to provide the <Identifier> for an
+// actual Ant colony.
+//
+// It also provide the interface to read/write <priv::Experiment> to
+// the filesystem.
+//
+// user may only to initialize a single object of this type, as
+// <priv::Ant> will be only uniquely identified by theyr
+// <myrmidon::Ant::ID> when they originate from the same
+// <Identifier>. Anyway dataset are very large and we should not try
+// to anlayse several of them in the same program.
 class Experiment {
 public :
+	// The AprilTag families supported by the FORT project.
+	//
+	// The AprilTag families supported by the FORT project.
+	// TODO: should it move to fort hermes ?
 	enum class TagFamily {Tag36h11=0,Tag36h10,Tag36ARTag,Tag16h5,Tag25h9,Circle21h7,Circle49h12,Custom48h12,Standard41h12,Standard52h13,Unset};
 
+	// Maps <TrackingDataDirectory> by their path
 	typedef std::unordered_map<std::string,TrackingDataDirectory> TrackingDataDirectoryByPath;
 
+	// A Pointer to an Experiment.
 	typedef std::unique_ptr<Experiment> Ptr;
 
+	// Opens an existing experiment given its fs::path
+	// @filename the fs::path to the ".myrmidon" file
+	// @return a <Ptr> to the <Experiment>.
 	static Ptr Open(const fs::path & filename);
+
+	// Creates a new <Experiment> given a fs::path
+	// @filename the fs::path to the ".myrmidon" file
+	//
+	// Creates a new <Experiment> associated with the
+	// <fs::path>. **This function does not create any file on
+	// itself.** Use either <NewFile> or <Save>
+	// @return a <Ptr> to the empty <Experiment>
 	static Ptr Create(const fs::path & filename);
+
+	// Creates a new <Experiment> on the filesystem.
+	// @filename the fs::path to the ".myrmidon" file
+	//
+	// Creates a new experiment and save it on a new file.
+	// TODO: Is it too much. mconsider removing it.
+	// @return a <Ptr> to the empty <Experiment>
 	static Ptr NewFile(const fs::path & filename);
+
+	// Saves te Experiment to the filesystem
+	// @filename the path to save too
+	//
+	//  Saves the <Experiment> to the filesystem. It consists only of
+	//  lightweight metadata of each Ants, Identification, Measurement
+	//  shapes. The actual tracking data is kept in the actual
+	//  directory referred byt the TrackingDatadirectory.
+	//
+	// Saves the <priv::Experiment> data to the filesystem
 	void Save(const fs::path & filename) const;
 
+
+	// The absolute path of the Experiment
+	// @return the absolute fs::path of the <priv::Experiment> on the
+	//         filesysten
 	const fs::path & AbsolutePath() const;
+
+	// The parent dir of the Experiment
+	//
+	// Since <TrackingDataDirectory> are pointer to the actual data
+	// directory on the filesystem, any Path is relative to this
+	// <Basedir>
+	// @return a fs::path to the base directory of this
+	//         <priv::Experiment>.
 	const fs::path & Basedir() const;
 
+
+	// Adds a new TrackingDataDirectory
+	// @tdd the new <TrackingDataDirectory> to add
+	//
+	// This methods adds the <TrackingDataDirectory> only if none of
+	// its Frame overlaps in time with the <TrackingDataDirectory>
+	// already referenced by this <priv::Experiment>.
+	//
+	// TODO: how to treat the case of multiple box experiment? In that
+	// case the frame will overlap. But its the same colony. But we
+	// have now two reference systems.
 	void AddTrackingDataDirectory(const TrackingDataDirectory & tdd);
+
+
+	// Removes a TrackingDataDirectory
+	// @path relative path to the directory
 	void RemoveTrackingDataDirectory(fs::path path);
 
+	// Gets the TrackingDataDirectory related to this Experiment
+	// @return a map of all <TrackingDataDirectory> related to this
+	//         <Experiment>.
 	const TrackingDataDirectoryByPath & TrackingDataDirectories() const;
 
+	// Accessor to the underlying Identifier
+	// @return a reference to the underlying <Identifier>
 	inline fort::myrmidon::priv::Identifier &  Identifier() {
 		return *d_identifier;
 	}
 
+	// ConstAccessor to the underlying Identifier
+	// @return a reference to the underlying <Identifier>
 	const fort::myrmidon::priv::Identifier & ConstIdentifier() const {
 		return *d_identifier;
 	}
@@ -71,6 +150,23 @@ public :
 	uint8_t Threshold() const;
 	void SetThreshold(uint8_t th);
 
+	// Found the largest time range where a <TagID> is unused.
+	// @start sets the first frame where the tag is unused, or an
+	//        empty pointer if the tag isn't used before <f>
+	// @end   set the last frame where the tag is unused, or an
+	//        empty pointer if the tag isn't used after <f>
+	// @tag the <TagID> to inquire for
+	// @f the <FramePointer> designating the point in time we want a free range.
+	// @return true if such a range was found, false if <t> is already used at time <f>
+	//
+	// Try to find the largest range where the <t> is not used,
+	// containing the Frame <f>. If the tag is actually used at this
+	// frame returns false. Otherwise returns true and sets <start>
+	// and <end> accordingly. Note that a reset pointer means that the
+	// tag was not used before or after t.
+	//
+	// TODO: this logic clearly is owned by the Identifier, move it
+	// here.
 	bool FreeRangeContaining(FramePointerPtr & start,
 	                         FramePointerPtr & end,
 	                         uint32_t tag, const FramePointer & f) const;
