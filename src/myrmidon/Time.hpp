f901b624a453a825bf5a0db1c83f4276ae799377
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-10-31 21:57:26 +0100

    Stubs Time interface

--- src/myrmidon/Time.hpp
+++ src/myrmidon/Time.hpp
@@ -47,6 +47,57 @@ const static Duration Millisecond(1e6);
 const static Duration Microsecond(1e3);
 const static Duration Nanosecond(1);
 
+// Time represent a point in Time
+//
+// Time represent a point in Time. Why re-implementing one of this
+// object and not using a `struct timeval` or a `std::chrono`. We are
+// dealing with long living experiment on heterogenous system. Under
+// this circunstances, we would like also to measure precise time
+// difference. For this purpose we could use the framegrabber
+// monolotic clock, which is timestamping every frame we acquired.
+//
+// Well the issue is that we cannot solely rely on this clock, as we
+// may have several computers each with their own monolithic clock. Or
+// even with a single computer, every time we started the tracking we
+// must assume a new monotonic clock.
+//
+// We could use the wall clock but this clock may be resetted any
+// time, and we would end up with issue where a time difference
+// between two consecutive frame could be negative.
+//
+// Inspired from golang [time.Time](https://golang.org/pkg/time) we
+// propose an Object that store both a Wall time, and a Monotonic
+// timestamp. But here we could have different monotonic timestamp. We
+// try, whenever its possible (both Time have a monotonic time, and
+// they are issued from the same monotonic clock), use that value for
+// Time difference and Comparison. Otherwise the Wall clock value will
+// be used with the issue regarding the jitter or Wall clock reset.
+//
+// Every time are considered UTC.
+class Time {
+	typedef uint64_t MonoclockID;
+	static Time Now();
+	static Time Date(int year, int month, int day, int hour, int minute, int seconds, int nanoseconds);
+	static Time FromTimeT();
+	static Time FromTimestamp();
+
+	Time();
+	Time(,uint64_t monoNanoseconds,MonoclockID ID);
+	Time(int64_t seconds,int32_t nanoseconds,uint64_t monoNanoseconds,MonoclockID ID);
+
+	Time Add(const Duration & d);
+	bool After(const Time & t) const;
+	bool Before(const Time & t) const;
+	Duration Sub(const Time & time) const;
+
+
+private:
+	uint64_t    d_wall;
+	uint64_t    d_monotonic;
+	MonoclockID d_monotonicID;
+};
+
+
 } // namespace myrmidon
 
 } // namespace fort
