5f4bf563a1d01ee6220b0f99d2cddce1ac4e87a0
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-01-31 16:13:45 +0100

    Adds some unit test on protobuf file IO
    Cannot test it to 100%

--- src/myrmidon/priv/proto/FileReadWriterUTest.cpp
+++ src/myrmidon/priv/proto/FileReadWriterUTest.cpp
@@ -1,1 +1,36 @@
 #include "FileReadWriterUTest.hpp"
+
+
+#include <myrmidon/ExperimentFile.pb.h>
+#include <myrmidon/priv/proto/FileReadWriter.hpp>
+#include <myrmidon/TestSetup.hpp>
+namespace fort {
+namespace myrmidon {
+namespace priv {
+namespace proto {
+
+TEST_F(FileReadWriterUTest,TestBadIO) {
+	typedef FileReadWriter<pb::FileHeader,pb::FileLine> RW;
+
+	pb::FileHeader h;
+
+	EXPECT_THROW({
+			RW::Write(TestSetup::Basedir() / "does-no-exist-dir"/ "foo.myrmidon",
+			          h,{
+			          });
+		},std::runtime_error);
+
+	EXPECT_THROW({
+			RW::Read(TestSetup::Basedir() / "does-no-exist-dir"/ "foo.myrmidon",
+			         [](const pb::FileHeader & h) {
+			         },
+			         [](const pb::FileLine & line) {
+			         });
+		},std::runtime_error);
+
+}
+
+} //namespace proto
+} //namespace priv
+} //namespace myrmidon
+} //namespace fort
