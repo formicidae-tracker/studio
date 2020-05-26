2c0b8d08b703edc0d5194c65db446811522e020d
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-05-26 16:47:57 +0200

    Implements Zone

--- bindings/R/fm/src/zone.cpp
+++ bindings/R/fm/src/zone.cpp
@@ -2,7 +2,13 @@
 #include "shapes.h"
 #include "time.h"
 
+namespace Rcpp {
+template <> SEXP wrap(const fort::myrmidon::ZoneDefinition::ConstList & );
+template <> SEXP wrap(const fort::myrmidon::ZoneDefinition::List & );
+}
+
 #include <Rcpp.h>
+
 
 void fmCZoneDefinition_show(const fort::myrmidon::CZoneDefinition * zd) {
 	Rcpp::Rcout << "fmZoneDefinition ([\n";
@@ -45,20 +51,56 @@ RCPP_MODULE(zone) {
 	Rcpp::class_<fort::myrmidon::CZoneDefinition>("fmCZoneDefinition")
 		.const_method("show",&fmCZoneDefinition_show)
 		.const_method("geometry",&fort::myrmidon::CZoneDefinition::Geometry)
+		.const_method("start",&fort::myrmidon::CZoneDefinition::Start)
+		.const_method("end",&fort::myrmidon::CZoneDefinition::End)
 		;
 
 	Rcpp::class_<fort::myrmidon::ZoneDefinition>("fmZoneDefinition")
 		.const_method("show",&fmZoneDefinition_show)
 		.const_method("geometry",&fort::myrmidon::ZoneDefinition::Geometry)
 		.method("setGeometry",&fort::myrmidon::ZoneDefinition::SetGeometry)
+		.const_method("start",&fort::myrmidon::ZoneDefinition::Start)
+		.const_method("end",&fort::myrmidon::ZoneDefinition::End)
+		.method("setStart",&fort::myrmidon::ZoneDefinition::SetStart)
+		.method("setEnd",&fort::myrmidon::ZoneDefinition::SetEnd)
 		;
 
 	Rcpp::class_<fort::myrmidon::CZone>("fmCZone")
 		.const_method("show",&fmCZone_show)
+		.const_method("cDefinitions",&fort::myrmidon::CZone::CDefinitions)
+		.const_method("name",&fort::myrmidon::CZone::Name)
+		.const_method("zoneID",&fort::myrmidon::CZone::ZoneID)
 		;
 
 	Rcpp::class_<fort::myrmidon::Zone>("fmZone")
 		.const_method("show",&fmZone_show)
+		.method("addDefinition",&fort::myrmidon::Zone::AddDefinition)
+		.const_method("cDefinitions",&fort::myrmidon::Zone::CDefinitions)
+		.method("definitions",&fort::myrmidon::Zone::Definitions)
+		.method("eraseDefinition",&fort::myrmidon::Zone::EraseDefinition)
+		.const_method("name",&fort::myrmidon::Zone::Name)
+		.method("setName",&fort::myrmidon::Zone::SetName)
+		.const_method("zoneID",&fort::myrmidon::Zone::ZoneID)
 		;
+
+}
+
+namespace Rcpp {
+
+template <> SEXP wrap(const fort::myrmidon::ZoneDefinition::ConstList & l) {
+	List res;
+	for ( const auto & zd : l ) {
+		res.push_back(zd);
+	}
+	return res;
+}
+
+template <> SEXP wrap(const fort::myrmidon::ZoneDefinition::List & l ) {
+	List res;
+	for ( const auto & zd : l ) {
+		res.push_back(zd);
+	}
+	return res;
+}
 
 }
