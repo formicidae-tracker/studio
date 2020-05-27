5d954c4e31676d5cf13f36d0a55659d5d7a75b22
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-05-27 21:09:43 +0200

    Implemenst functor query version

--- src/fort/myrmidon/Query.cpp
+++ src/fort/myrmidon/Query.cpp
@@ -4,8 +4,26 @@
 
 #include "priv/Query.hpp"
 
+
+
 namespace fort {
 namespace myrmidon {
+
+
+template <typename T>
+class WrapFunctor {
+public:
+	typedef std::function<void(const T &)> StoreFunction;
+	WrapFunctor(const StoreFunction & functor)
+		: d_functor(functor) {
+	}
+	inline void operator=(const T & t) const {
+		d_functor(t);
+	}
+private:
+	StoreFunction d_functor;
+};
+
 
 ComputedMeasurement::List Query::ComputeMeasurementFor(const CExperiment & experiment,
                                                        Ant::ID antID,
@@ -22,12 +40,32 @@ TagStatistics::ByTagID Query::ComputeTagStatistics(const CExperiment & experimen
 }
 
 
+void Query::IdentifyFramesFunctor(const CExperiment & experiment,
+                                  std::function<void (const IdentifiedFrame::ConstPtr &)> storeData,
+                                  const Time::ConstPtr & start,
+                                  const Time::ConstPtr & end,
+                                  bool computeZones) {
+	WrapFunctor inserter(storeData);
+	priv::Query::IdentifyFrames(experiment.d_p,inserter,start,end,computeZones);
+}
+
+
 void Query::IdentifyFrames(const CExperiment & experiment,
                            std::vector<IdentifiedFrame::ConstPtr> & result,
                            const Time::ConstPtr & start,
-                           const Time::ConstPtr & end) {
+                           const Time::ConstPtr & end,
+                           bool computeZones) {
 	auto backInserter = std::back_inserter(result);
-	priv::Query::IdentifyFrames(experiment.d_p,backInserter,start,end);
+	priv::Query::IdentifyFrames(experiment.d_p,backInserter,start,end,computeZones);
+}
+
+
+void Query::CollideFramesFunctor(const CExperiment & experiment,
+                                 std::function<void (const CollisionData & data)> storeData,
+                                 const Time::ConstPtr & start,
+                                 const Time::ConstPtr & end) {
+	WrapFunctor backInserter(storeData);
+	priv::Query::CollideFrames(experiment.d_p,backInserter,start,end);
 }
 
 
@@ -39,20 +77,61 @@ void Query::CollideFrames(const CExperiment & experiment,
 	priv::Query::CollideFrames(experiment.d_p,backInserter,start,end);
 }
 
+void Query::ComputeTrajectoriesFunctor(const CExperiment & experiment,
+                                       std::function<void (const AntTrajectory::ConstPtr &)> storeTrajectory,
+                                       const Time::ConstPtr & start,
+                                       const Time::ConstPtr & end,
+                                       Duration maximumGap,
+                                       Matcher::Ptr matcher,
+                                       bool computeZones) {
+	WrapFunctor backInserter(storeTrajectory);
+	priv::Query::ComputeTrajectories(experiment.d_p,
+	                                 backInserter,
+	                                 start,
+	                                 end,
+	                                 maximumGap,
+	                                 matcher->d_p,
+	                                 computeZones);
+}
+
+
+
 void Query::ComputeTrajectories(const CExperiment & experiment,
                                 std::vector<AntTrajectory::ConstPtr> & trajectories,
                                 const Time::ConstPtr & start,
                                 const Time::ConstPtr & end,
                                 Duration maximumGap,
-                                Matcher::Ptr matcher) {
+                                Matcher::Ptr matcher,
+                                bool computeZones) {
 	auto backInserter = std::back_inserter(trajectories);
 	priv::Query::ComputeTrajectories(experiment.d_p,
 	                                 backInserter,
 	                                 start,
 	                                 end,
 	                                 maximumGap,
-	                                 matcher->d_p);
+	                                 matcher->d_p,
+	                                 computeZones);
 }
+
+void Query::ComputeAntInteractionsFunctor(const CExperiment & experiment,
+                                          std::function<void ( const AntTrajectory::ConstPtr&)> storeTrajectory,
+                                          std::function<void ( const AntInteraction::ConstPtr&)> storeInteraction,
+                                          const Time::ConstPtr & start,
+                                          const Time::ConstPtr & end,
+                                          Duration maximumGap,
+                                          Matcher::Ptr matcher) {
+	WrapFunctor trajInserter(storeTrajectory);
+	WrapFunctor interInserter(storeInteraction);
+
+	priv::Query::ComputeAntInteractions(experiment.d_p,
+	                                    trajInserter,
+	                                    interInserter,
+	                                    start,
+	                                    end,
+	                                    maximumGap,
+	                                    matcher->d_p);
+}
+
 
 void Query::ComputeAntInteractions(const CExperiment & experiment,
                                    std::vector<AntTrajectory::ConstPtr> & trajectories,
--- src/fort/myrmidon/Query.hpp
+++ src/fort/myrmidon/Query.hpp
@@ -36,6 +36,26 @@ public:
 	// @return the tag statistics index by <TagID>
 	static TagStatistics::ByTagID ComputeTagStatistics(const CExperiment & experiment);
 
+
+	// Identifies ants in frames - functor version
+	// @experiment the <Experiment> to query for
+	// @storeData a functor to store/convert the data
+	// @start the start time for the query use nullptr for the starts
+	//        of the experiment.
+	// @end the end time for the query, use nullptr for the end of the
+	//      experiment
+	// @computeZones should compute zones for, makes computation slower
+	//
+	// Identifies Ants in frames, data will be reported ordered by
+	// time.  This version aimed to be used by language bindings to
+	// avoid large data copy.
+	static void IdentifyFramesFunctor(const CExperiment & experiment,
+	                                  std::function<void (const IdentifiedFrame::ConstPtr &)> storeData,
+	                                  const Time::ConstPtr & start,
+	                                  const Time::ConstPtr & end,
+	                                  bool computeZones = false);
+
+
 	// Identifies ants in frames
 	// @experiment the <Experiment> to query for
 	// @result the resulting <IdentifiedFrame>
@@ -43,12 +63,32 @@ public:
 	//        of the experiment.
 	// @end the end time for the query, use nullptr for the end of the
 	//      experiment
+	// @computeZones should compute zones for, makes computation slower
 	//
 	// Identifies Ants in frames, data will be reported ordered by time.
 	static void IdentifyFrames(const CExperiment & experiment,
 	                           std::vector<IdentifiedFrame::ConstPtr> & result,
 	                           const Time::ConstPtr & start,
-	                           const Time::ConstPtr & end);
+	                           const Time::ConstPtr & end,
+	                           bool computeZones = false);
+
+	// Finds <Collision> in data frame - functor version
+	// @OutputIter an output iterator to fill results
+	// @experiment the <Experiment> to query for
+	// @storeData a functor to store the data as it is produced
+	// @start the start time for the query use nullptr for the starts
+	//        of the experiment.
+	// @end the end time for the query, use nullptr for the end of the
+	//      experiment
+	//
+	// Finds <Collision> between ants in frames, data will be reported
+	// ordered by time. This version aimed to be used by language bindings to
+	// avoid large data copy.
+	static void CollideFramesFunctor(const CExperiment & experiment,
+	                                 std::function<void (const CollisionData & data)> storeData,
+	                                 const Time::ConstPtr & start,
+	                                 const Time::ConstPtr & end);
+
 
 	// Finds <Collision> in data frame
 	// @experiment the <Experiment> to query for
@@ -65,6 +105,32 @@ public:
 	                          const Time::ConstPtr & start,
 	                          const Time::ConstPtr & end);
 
+	// Computes trajectories for ants - functor version
+	// @experiment the <Experiment> to query for
+	// @storeTrajectory a functor to store/covert the data
+	// @start the start time for the query use nullptr for the starts
+	//        of the experiment.
+	// @end the end time for the query, use nullptr for the end of the
+	//      experiment
+	// @maximumGap the maximal undetected duration before cutting the
+	//             trajectory in two
+	// @matcher a <Matcher> to specify more precise, less memory
+	//          intensive queries.
+	// @computeZones enables ant zone computation, but slower query
+	//
+	// Computes trajectories for <Ant>. Those will be reported ordered
+	// by ending time. This version aimed to be used by language bindings to
+	// avoid large data copy.
+	static void ComputeTrajectoriesFunctor(const CExperiment & experiment,
+	                                       std::function<void (const AntTrajectory::ConstPtr &)> storeTrajectory,
+	                                       const Time::ConstPtr & start,
+	                                       const Time::ConstPtr & end,
+	                                       Duration maximumGap,
+	                                       Matcher::Ptr matcher = Matcher::Ptr(),
+	                                       bool computeZones = false);
+
+
+
 	// Computes trajectories for ants
 	// @experiment the <Experiment> to query for
 	// @trajectories the resulting <IdentifiedFrame> and <CollisionFrame>
@@ -76,6 +142,7 @@ public:
 	//             trajectory in two
 	// @matcher a <Matcher> to specify more precise, less memory
 	//          intensive queries.
+	// @computeZones enables ant zone computation, but slower query
 	//
 	// Computes trajectories for <Ant>. Those will be reported ordered
 	// by ending time
@@ -84,7 +151,35 @@ public:
 	                                const Time::ConstPtr & start,
 	                                const Time::ConstPtr & end,
 	                                Duration maximumGap,
-	                                Matcher::Ptr matcher = Matcher::Ptr());
+	                                Matcher::Ptr matcher = Matcher::Ptr(),
+	                                bool computeZones = false );
+
+
+	// Computes interactions for ants - functor version
+	// @experiment the <Experiment> to query for
+	// @storeTrajectory a functor to store/convert trajectories
+	// @storeInteraction  a functor to store/convert interaction
+	// @start the start time for the query use nullptr for the starts
+	//        of the experiment.
+	// @end the end time for the query, use nullptr for the end of the
+	//      experiment
+	// @maximumGap the maximal undetected duration before cutting the
+	//             trajectory in two
+	// @matcher a <Matcher> to specify more precise, less memory
+	//          intensive queries.
+	//
+	// Computes interactions for <Ant>. Those will be reported ordered
+	// by ending time. This version aimed to be used by language bindings to
+	// avoid large data copy.
+	static void ComputeAntInteractionsFunctor(const CExperiment & experiment,
+	                                          std::function<void ( const AntTrajectory::ConstPtr&)> storeTrajectory,
+	                                          std::function<void ( const AntInteraction::ConstPtr&)> storeInteraction,
+	                                          const Time::ConstPtr & start,
+	                                          const Time::ConstPtr & end,
+	                                          Duration maximumGap,
+	                                          Matcher::Ptr matcher = Matcher::Ptr());
+
+
 
 	// Computes interactions for ants
 	// @experiment the <Experiment> to query for
