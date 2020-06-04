ab9e10634197837500dfb3ddf669f5f035676963
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-06-04 18:32:53 +0200

    Documents fmColor

--- bindings/R/FortMyrmidon/R/AntStaticValue.R
+++ bindings/R/FortMyrmidon/R/AntStaticValue.R
@@ -5,6 +5,7 @@
 #'     functioanility of these objects. since we cannot mimic the use
 #'     of \code{std::get<TYPE>()} function, these R object uses the
 #'     following methods.
+#'     \cr\cr
 #'     To create an fmStaticValue, please use the following function
 #'     to create an object of the appropriate type:
 #'     \itemize{
@@ -14,6 +15,7 @@
 #'     \item \code{\link{fmAntStaticString}}
 #'     \item \code{\link{fmAntStaticTime}}
 #'     }
+#'     \cr\cr
 #'     A \code{fmAntStaticValue} as an assigned type, declared by
 #'     \code{\link{fmAntMetadataType}}. It is not possible to convert
 #'     autonatically one type to another (you have to do it manually).
@@ -33,29 +35,37 @@ NULL
 #' @name fmAntStaticBool
 #' @title creates a \code{\link{fmAntStaticValue}} from a boolean
 #' @param v the boolean to represent
-#' @return a \code{\link{fmAntStaticValue}} of type \code{BOOL}, see \code{\link{fmAntMetadataType}}
+#' @return a \code{\link{fmAntStaticValue}} of type \code{BOOL}, see
+#'     \code{\link{fmAntMetadataType}}
 NULL
 
 #' @name fmAntStaticInteger
 #' @title creates a \code{\link{fmAntStaticValue}} from an integer
 #' @param v the integer to represent
-#' @return a \code{\link{fmAntStaticValue}} of type \code{INTEGER}, see \code{\link{fmAntMetadataType}}
+#' @return a \code{\link{fmAntStaticValue}} of type \code{INTEGER},
+#'     see \code{\link{fmAntMetadataType}}
 NULL
 
 #' @name fmAntStaticNumeric
-#' @title creates a \code{\link{fmAntStaticValue}} from a numerical value
+#' @title creates a \code{\link{fmAntStaticValue}} from a numerical
+#'     value
 #' @param v the numeric to represent
-#' @return a \code{\link{fmAntStaticValue}} of type \code{DOUBLE}, see \code{\link{fmAntMetadataType}}
+#' @return a \code{\link{fmAntStaticValue}} of type \code{DOUBLE}, see
+#'     \code{\link{fmAntMetadataType}}
 NULL
 
 #' @name fmAntStaticString
-#' @title creates a \code{\link{fmAntStaticValue}} from a character vector
+#' @title creates a \code{\link{fmAntStaticValue}} from a character
+#'     vector
 #' @param v the character vector to represent
-#' @return a \code{\link{fmAntStaticValue}} of type \code{STRING}, see \code{\link{fmAntMetadataType}}
+#' @return a \code{\link{fmAntStaticValue}} of type \code{STRING}, see
+#'     \code{\link{fmAntMetadataType}}
 NULL
 
 #' @name fmAntStaticTime
-#' @title creates a \code{\link{fmAntStaticValue}} from a \code{\link{fmTime}}
+#' @title creates a \code{\link{fmAntStaticValue}} from a
+#'     \code{\link{fmTime}}
 #' @param v the \code{\link{fmTime}} to represent
-#' @return a \code{\link{fmAntStaticValue}} of type \code{TIME}, see \code{\link{fmAntMetadataType}}
+#' @return a \code{\link{fmAntStaticValue}} of type \code{TIME}, see
+#'     \code{\link{fmAntMetadataType}}
 NULL
