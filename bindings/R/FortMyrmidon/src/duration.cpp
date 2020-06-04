cc21081e21c9fc0cf9de0b17da817c255f394e79
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-06-04 16:23:44 +0200

    Updates R documentation

--- bindings/R/FortMyrmidon/src/duration.cpp
+++ bindings/R/FortMyrmidon/src/duration.cpp
@@ -2,30 +2,31 @@
 
 #include "Rcpp.h"
 
+
+
 // Helpers method for Duration
 void fmDuration_show(fort::myrmidon::Duration * d) {
 	Rcpp::Rcout << *d << "\n";
 }
 
-
-fort::myrmidon::Duration fmHour(int64_t v) {
-	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Hour);
+fort::myrmidon::Duration fmHour(double v) {
+	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Hour.Nanoseconds());
 }
 
-fort::myrmidon::Duration fmMinute(int64_t v) {
-	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Minute);
+fort::myrmidon::Duration fmMinute(double v) {
+	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Minute.Nanoseconds());
 }
 
-fort::myrmidon::Duration fmSecond(int64_t v) {
-	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Second);
+fort::myrmidon::Duration fmSecond(double v) {
+	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Second.Nanoseconds());
 }
 
-fort::myrmidon::Duration fmMillisecond(int64_t v) {
-	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Millisecond);
+fort::myrmidon::Duration fmMillisecond(double v) {
+	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Millisecond.Nanoseconds());
 }
 
-fort::myrmidon::Duration fmMicrosecond(int64_t v) {
-	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Microsecond);
+fort::myrmidon::Duration fmMicrosecond(double v) {
+	return fort::myrmidon::Duration(v * fort::myrmidon::Duration::Microsecond.Nanoseconds());
 }
 
 fort::myrmidon::Duration fmNanosecond(int64_t v) {
@@ -37,12 +38,18 @@ RCPP_MODULE(duration) {
 	Rcpp::class_<fort::myrmidon::Duration>("fmDuration")
 		.constructor()
 		.constructor<int64_t>()
-		.method("hours",&fort::myrmidon::Duration::Hours)
-		.method("minutes",&fort::myrmidon::Duration::Minutes)
-		.method("seconds",&fort::myrmidon::Duration::Seconds)
-		.method("milliseconds",&fort::myrmidon::Duration::Milliseconds)
-		.method("microseconds",&fort::myrmidon::Duration::Microseconds)
-		.method("nanoseconds",&fort::myrmidon::Duration::Nanoseconds)
+		.method("hours",
+		        &fort::myrmidon::Duration::Hours)
+		.method("minutes",
+		        &fort::myrmidon::Duration::Minutes)
+		.method("seconds",
+		        &fort::myrmidon::Duration::Seconds)
+		.method("milliseconds",
+		        &fort::myrmidon::Duration::Milliseconds)
+		.method("microseconds",
+		        &fort::myrmidon::Duration::Microseconds)
+		.method("nanoseconds",
+		        &fort::myrmidon::Duration::Nanoseconds)
 		.method("show",&fmDuration_show)
 		;
 	Rcpp::function("fmHour",&fmHour);
