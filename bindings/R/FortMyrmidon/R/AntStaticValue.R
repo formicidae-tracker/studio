8307b7f753d3b666f04b86a36e50270d793ebcf5
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-06-04 18:16:18 +0200

    Adds documenation for R fmAntStaticValue

---
+++ bindings/R/FortMyrmidon/R/AntStaticValue.R
@@ -0,0 +1,61 @@
+#' @name fmAntStaticValue
+#' @title a wrapper for \code{fort::myrmidon::AntStaticValue}
+#' @description \code{fort::myrmidon::AntStaticValue} are using C++17
+#'     a \code{std::variant}. This wrapper aims to emulate the
+#'     functioanility of these objects. since we cannot mimic the use
+#'     of \code{std::get<TYPE>()} function, these R object uses the
+#'     following methods.
+#'     To create an fmStaticValue, please use the following function
+#'     to create an object of the appropriate type:
+#'     \itemize{
+#'     \item \code{\link{fmAntStaticBool}}
+#'     \item \code{\link{fmAntStaticInteger}}
+#'     \item \code{\link{fmAntStaticNumeric}}
+#'     \item \code{\link{fmAntStaticString}}
+#'     \item \code{\link{fmAntStaticTime}}
+#'     }
+#'     A \code{fmAntStaticValue} as an assigned type, declared by
+#'     \code{\link{fmAntMetadataType}}. It is not possible to convert
+#'     autonatically one type to another (you have to do it manually).
+#'
+#' @field toBool cast the object to a boolean value, fails if the type
+#'     is not \code{BOOL}
+#' @field toInt cast the object to an integer value, fails if the type
+#'     is not \code{INTEGER}
+#' @field toNumeric cast the object to a numeric value, fails if the
+#'     type is not \code{DOUBLE}
+#' @field toString cast the object to a character value, fails if the
+#'     type is not \code{STRING}
+#' @field toTime cast the object to a \code{\link{fmTime}} value,
+#'     fails if the type is not \code{TIME}
+NULL
+
+#' @name fmAntStaticBool
+#' @title creates a \code{\link{fmAntStaticValue}} from a boolean
+#' @param v the boolean to represent
+#' @return a \code{\link{fmAntStaticValue}} of type \code{BOOL}, see \code{\link{fmAntMetadataType}}
+NULL
+
+#' @name fmAntStaticInteger
+#' @title creates a \code{\link{fmAntStaticValue}} from an integer
+#' @param v the integer to represent
+#' @return a \code{\link{fmAntStaticValue}} of type \code{INTEGER}, see \code{\link{fmAntMetadataType}}
+NULL
+
+#' @name fmAntStaticNumeric
+#' @title creates a \code{\link{fmAntStaticValue}} from a numerical value
+#' @param v the numeric to represent
+#' @return a \code{\link{fmAntStaticValue}} of type \code{DOUBLE}, see \code{\link{fmAntMetadataType}}
+NULL
+
+#' @name fmAntStaticString
+#' @title creates a \code{\link{fmAntStaticValue}} from a character vector
+#' @param v the character vector to represent
+#' @return a \code{\link{fmAntStaticValue}} of type \code{STRING}, see \code{\link{fmAntMetadataType}}
+NULL
+
+#' @name fmAntStaticTime
+#' @title creates a \code{\link{fmAntStaticValue}} from a \code{\link{fmTime}}
+#' @param v the \code{\link{fmTime}} to represent
+#' @return a \code{\link{fmAntStaticValue}} of type \code{TIME}, see \code{\link{fmAntMetadataType}}
+NULL
---
+++ bindings/R/FortMyrmidon/R/Experiment.R
@@ -0,0 +1,6 @@
+#' Enum list for \code{fort::myrmidon::AntMetadataType}
+#' @description list that represent the
+#'     \href{https://formicidae-tracker.github.io/studio/docs/latest/api/#fort::myrmidon/fort::myrmidon::AntMetadataType}{fort::myrmidon::AntMetadataType}
+#'     enum
+#' @usage names(which( mdTypeValue == fmAntMetadataType) )
+fmAntMetadataType <- list ( BOOL = 0L, INT = 1L, DOUBLE = 2L, STRING = 3L, TIME = 4L)
