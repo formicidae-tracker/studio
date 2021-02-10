795f2ee8e54a8f6a9f4228123589dcdf50c3ad37
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-02-10 15:51:25 +0100

    Enables ant clone action when possible
    Closes #180

--- src/fort/studio/utest/AntGeometryWorkspace.cpp
+++ src/fort/studio/utest/AntGeometryWorkspace.cpp
@@ -9,6 +9,7 @@
 #include <fort/studio/bridge/ExperimentBridge.hpp>
 #include <fort/studio/bridge/IdentifierBridge.hpp>
 #include <fort/studio/bridge/AntShapeTypeBridge.hpp>
+#include <fort/studio/bridge/AntShapeBridge.hpp>
 
 
 void WorkspaceUTest::SetUp(const std::string & name) {
@@ -47,7 +48,7 @@ void AntShapeWorkspaceUTest::SetUp() {
 		      Eigen::Vector2d(-1,-1),
 		      Eigen::Vector2d(-1,1)})));
 
-
+	bridge->selectAnt(1);
 
 }
 
@@ -62,4 +63,12 @@ TEST_F(AntShapeWorkspaceUTest,AddCapsuleActionState) {
 	EXPECT_EQ(shapeWs->insertAction()->isEnabled(),false);
 	shapeWs->typeBox()->setCurrentIndex(0);
 	EXPECT_EQ(shapeWs->insertAction()->isEnabled(),true);
+}
+
+TEST_F(AntShapeWorkspaceUTest,CloneShapeAction) {
+	EXPECT_EQ(shapeWs->cloneAntShapeAction()->isEnabled(),false) << "Should be disabled if current ant has no shape";
+	bridge->antShapes()->addCapsule(1,1,std::make_shared<fmp::Capsule>(Eigen::Vector2d(0,-1),
+	                                                                   Eigen::Vector2d(0,1),
+	                                                                   0.3,0.3));
+	EXPECT_EQ(shapeWs->cloneAntShapeAction()->isEnabled(),true) << "Should be enabled if current ant has a shape";
 }
