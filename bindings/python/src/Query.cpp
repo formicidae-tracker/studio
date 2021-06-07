0adcd7d216ae6461923fb062f44fe11fc1c89ae6
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-06-07 16:55:49 +0200

    Binds all Queries

--- bindings/python/src/Query.cpp
+++ bindings/python/src/Query.cpp
@@ -43,6 +43,64 @@ py::list QueryCollideFrames(const fort::myrmidon::Experiment & experiment,
 }
 
 
+py::list QueryComputeAntTrajectories(const fort::myrmidon::Experiment & experiment,
+                                     fort::Time start,
+                                     fort::Time end,
+                                     fort::Duration maximumGap,
+                                     const fort::myrmidon::Matcher::Ptr & matcher,
+                                     bool computeZones,
+                                     bool singleThreaded) {
+
+	py::list res;
+	fort::myrmidon::Query::ComputeAntTrajectoriesArgs args;
+	args.Start = start;
+	args.End = end;
+	args.MaximumGap = maximumGap;
+	args.Matcher = matcher;
+	args.ComputeZones = computeZones;
+	args.SingleThreaded = singleThreaded;
+	args.AllocationInCurrentThread = false;
+	fort::myrmidon::Query::ComputeAntTrajectoriesFunctor(experiment,
+	                                                     [&res](const fort::myrmidon::AntTrajectory::Ptr & t) {
+		                                                     res.append(t);
+	                                                     },
+	                                                     args);
+	return res;
+}
+
+py::tuple QueryComputeAntInteractions(const fort::myrmidon::Experiment & experiment,
+                                     fort::Time start,
+                                     fort::Time end,
+                                     fort::Duration maximumGap,
+                                     const fort::myrmidon::Matcher::Ptr & matcher,
+                                     bool reportFullTrajectories,
+                                     bool singleThreaded) {
+
+	py::list trajectories;
+	py::list interactions;
+
+	fort::myrmidon::Query::ComputeAntInteractionsArgs args;
+	args.Start = start;
+	args.End = end;
+	args.MaximumGap = maximumGap;
+	args.Matcher = matcher;
+	args.ReportFullTrajectories = reportFullTrajectories;
+	args.SingleThreaded = singleThreaded;
+	args.AllocationInCurrentThread = false;
+	fort::myrmidon::Query::ComputeAntInteractionsFunctor(experiment,
+	                                                     [&trajectories](const fort::myrmidon::AntTrajectory::Ptr & t) {
+		                                                     trajectories.append(t);
+	                                                     },
+	                                                     [&interactions](const fort::myrmidon::AntInteraction::Ptr & i) {
+		                                                     interactions.append(i);
+	                                                     },
+	                                                     args);
+	return py::make_tuple(trajectories,interactions);
+
+}
+
+
+
 void BindQuery(py::module_ & m) {
 	using namespace pybind11::literals;
 
@@ -75,8 +133,26 @@ void BindQuery(py::module_ & m) {
 		            "start"_a = collideArgs.Start,
 		            "end"_a = collideArgs.End,
 		            "singleThreaded"_a = collideArgs.SingleThreaded)
-
-
+		.def_static("ComputeAntTrajectories",
+		            &QueryComputeAntTrajectories,
+		            "experiment"_a,
+		            py::kw_only(),
+		            "start"_a = trajectoryArgs.Start,
+		            "end"_a = trajectoryArgs.End,
+		            "maximumGap"_a = trajectoryArgs.MaximumGap,
+		            "matcher"_a = trajectoryArgs.Matcher,
+		            "computeZones"_a = trajectoryArgs.ComputeZones,
+		            "singleThreaded"_a = trajectoryArgs.SingleThreaded)
+		.def_static("ComputeAntInteractions",
+		            &QueryComputeAntInteractions,
+		            "experiment"_a,
+		            py::kw_only(),
+		            "start"_a = trajectoryArgs.Start,
+		            "end"_a = trajectoryArgs.End,
+		            "maximumGap"_a = trajectoryArgs.MaximumGap,
+		            "matcher"_a = trajectoryArgs.Matcher,
+		            "reportFullTrajectories"_a = trajectoryArgs.ComputeZones,
+		            "singleThreaded"_a = trajectoryArgs.SingleThreaded)
 		;
 
 
