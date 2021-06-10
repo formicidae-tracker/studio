c145a66846a1a2e99f71040bcaaf4f31ca6071b2
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-06-10 16:03:27 +0200

    Unbinds `fort::myrmidon::AntStaticValue` in python
    There is a direct support in pybind11 for std::variant, and any object
    of the correct type can be used as argument/return values. No need to
    add a class for this.

--- bindings/python/src/Types.cpp
+++ bindings/python/src/Types.cpp
@@ -10,37 +10,6 @@ namespace py = pybind11;
 
 template<class> inline constexpr bool always_false_v = false;
 
-void BindAntStaticValue(py::module_ & m) {
-
-	py::class_<fort::myrmidon::AntStaticValue>(m,"AntStaticValue")
-		.def_static("Type",
-		            [](const fort::myrmidon::AntStaticValue & v) -> std::string {
-			            return std::visit([](auto&& arg) {
-				                              using T = std::decay_t<decltype(arg)>;
-				                              if constexpr (std::is_same_v<T, bool>)
-					                                           return "bool";
-				                              else if constexpr (std::is_same_v<T, int>)
-					                                                return "int";
-				                              else if constexpr (std::is_same_v<T, double>)
-					                                                return "float";
-				                              else if constexpr (std::is_same_v<T, std::string>)
-					                                                return "string";
-				                              else if constexpr (std::is_same_v<T, fort::Time>)
-					                                                return "py_fort_myrmidon.Time";
-				                              else
-					                              static_assert(always_false_v<T>, "non-exhaustive visitor!");
-			                              }, v);
-	      },"prints an AntStaticValue type as seen as C++ side (for unit testing)")
-		.def_static("TrueValue",[](){ return fort::myrmidon::AntStaticValue(true);},"C++ emitted boolean value (for unit testing)")
-		.def_static("IntValue",[](){ return fort::myrmidon::AntStaticValue(123);},"C++ emitted int value (for unit testing)")
-		.def_static("FloatValue",[](){ return fort::myrmidon::AntStaticValue(123.123);},"C++ emitted float value (for unit testing)")
-		.def_static("StringValue",[](){ return fort::myrmidon::AntStaticValue(std::string("string"));},"C++ emitted string value (for unit testing)")
-		.def_static("TimeValue",[](){ return fort::myrmidon::AntStaticValue(fort::Time());},"C++ emitted Time value (for unit testing)")
-		;
-
-
-
-}
 
 void BindComputedMeasurement(py::module_ & m) {
 	using namespace fort::myrmidon;
@@ -242,7 +211,6 @@ void BindExperimentDataInfo(py::module_ & m) {
 void BindTypes(py::module_ & m) {
 	BindColor(m);
 	BindTime(m);
-	BindAntStaticValue(m);
 	BindTagStatistics(m);
 	BindComputedMeasurement(m);
 	BindIdentifiedFrame(m);
